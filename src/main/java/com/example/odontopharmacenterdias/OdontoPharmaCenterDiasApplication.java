48c5d870d2cee95a003b08ae6e83a9755047f247
Pedro Amorim <amorimpedrocassio@gmail.com>
2024-06-11 22:18:01 -0300

    Iniating project

---
+++ src/main/java/com/example/odontopharmacenterdias/OdontoPharmaCenterDiasApplication.java
@@ -0,0 +1,13 @@
+package com.example.odontopharmacenterdias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdontoPharmaCenterDiasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdontoPharmaCenterDiasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/odontopharmacenterdias/OdontoPharmaCenterDiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.odontopharmacenterdias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdontoPharmaCenterDiasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
